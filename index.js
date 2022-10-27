8c17166ad18dd90b1f622277d32c8d95fdbbb38f
Evan Walter <evan.walter.software@gmail.com>
2022-10-26 18:14:04 -0700

    get all members

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ const path = require('path')
 
 const app = express()
 
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'Bob Williams',
+    email: 'bob@gmail.com',
+    status: 'inactive',
+  },
+  {
+    id: 3,
+    name: 'Shannon Jackson',
+    email: 'shannon@gmail.com',
+    status: 'active',
+  },
+]
+
+// Gets all members
+app.get('/api/members', (req, res) => res.json(members))
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
