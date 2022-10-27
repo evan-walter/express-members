c8dab9c520971a863b307fb964e9d6d962e9fac8
Evan Walter <evan.walter.software@gmail.com>
2022-10-26 20:35:23 -0700

    set up to create a member

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 const express = require('express')
 const path = require('path')
 const logger = require('./middleware/logger')
-const members = require('./Members')
 
 const app = express()
 
 // Init middleware
 // app.use(logger)
 
-// Gets all members
-app.get('/api/members', (req, res) => res.json(members))
-
-// Get single member
-app.get('/api/members/:id', (req, res) => {
-  const found = members.some((member) => member.id === parseInt(req.params.id))
-
-  if (found) {
-    res.json(members.filter((member) => member.id === parseInt(req.params.id)))
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
-  }
-})
-
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Members API routes
+app.use('/api/members', require('./routes/api/members'))
 
 const PORT = process.env.PORT || 5001
 
