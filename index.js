9afb2e38494e550717291bc257fa085ba0980eb2
Evan Walter <evan.walter.software@gmail.com>
2022-10-26 23:02:37 -0700

    set up handlebars

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
 const logger = require('./middleware/logger')
 
 const app = express()
@@ -7,9 +8,16 @@ const app = express()
 // Init middleware
 // app.use(logger)
 
+// Handlebars middleware
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 // Body Parser Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// Homepage route
+app.get('/', (req, res) => res.render('index'))
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
