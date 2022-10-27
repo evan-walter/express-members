61d001e5f355a28d7926cccffd7acf56082e7676
Evan Walter <evan.walter.software@gmail.com>
2022-10-26 18:03:53 -0700

    create static server

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const path = require('path')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5001
 
