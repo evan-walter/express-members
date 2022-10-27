d4d403b483aab8e8280c90373b260513071b0733
Evan Walter <evan.walter.software@gmail.com>
2022-10-26 18:20:18 -0700

    init middleware

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 const express = require('express')
 const path = require('path')
+const members = require('./Members')
 
 const app = express()
 
-const members = [
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'john@gmail.com',
-    status: 'active',
-  },
-  {
-    id: 2,
-    name: 'Bob Williams',
-    email: 'bob@gmail.com',
-    status: 'inactive',
-  },
-  {
-    id: 3,
-    name: 'Shannon Jackson',
-    email: 'shannon@gmail.com',
-    status: 'active',
-  },
-]
+const logger = (req, res, next) => {
+  console.log('Hello')
+  next()
+}
+
+// Init middleware
+app.use(logger)
 
 // Gets all members
 app.get('/api/members', (req, res) => res.json(members))
